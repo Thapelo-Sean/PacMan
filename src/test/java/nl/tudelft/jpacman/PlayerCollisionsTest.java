ea14f9c8679c14d3cf27ef903572f834c5f29bb3
Thapelo Sean <thapelomatji12@gmail.com>
2023-12-12 16:57:50 +0200

    Modified PlayerCollisionsTest.java class

--- src/test/java/nl/tudelft/jpacman/PlayerCollisionsTest.java
+++ src/test/java/nl/tudelft/jpacman/PlayerCollisionsTest.java
@@ -1,6 +1,81 @@
 package nl.tudelft.jpacman;
 
+import nl.tudelft.jpacman.board.Direction;
+import nl.tudelft.jpacman.level.Pellet;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.level.PlayerCollisions;
+import nl.tudelft.jpacman.npc.Ghost;
+import nl.tudelft.jpacman.points.PointCalculator;
+import nl.tudelft.jpacman.sprite.AnimatedSprite;
+import nl.tudelft.jpacman.sprite.EmptySprite;
+import nl.tudelft.jpacman.sprite.Sprite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import static org.mockito.Mockito.*;
+
 public class PlayerCollisionsTest {
 
+    private PlayerCollisions playerCollisions;
+    private PointCalculator pointCalculator;
+    private Player player;
+    private Ghost ghost;
+    private Pellet pellet;
+    private Sprite[] frames;
+    private Map<Direction, Sprite> spriteMap;
+    private AnimatedSprite deathAnimation;
 
+    @BeforeEach
+    void setUp() {
+        pointCalculator = mock(PointCalculator.class);
+        pellet = mock(Pellet.class);
+        playerCollisions = new PlayerCollisions(pointCalculator);
+        spriteMap = new HashMap<>();
+
+        //non-empty array of frames
+        frames = new Sprite[]{new EmptySprite(), new EmptySprite()};
+        deathAnimation = new AnimatedSprite(frames, 1, true);
+        player = new Player(spriteMap, deathAnimation);
+        ghost = new Ghost(spriteMap, 0, 0)
+        {
+            @Override
+            public Optional<Direction> nextAiMove() {
+                    return Optional.empty();
+            }
+        };
+    }
+
+
+    @Test
+    void testPlayerCollidingWithGhost() {
+        playerCollisions.collide(player, ghost);
+        verify(pointCalculator).collidedWithAGhost(player, ghost);
+        Assertions.assertFalse(player.isAlive());
+        Assertions.assertEquals(ghost, player.getKiller());
+    }
+
+    @Test
+    void testGhostCollidingWithPlayer() {
+        playerCollisions.collide(ghost, player);
+        verify(pointCalculator).collidedWithAGhost(player, ghost);
+        Assertions.assertFalse(player.isAlive());
+        Assertions.assertEquals(ghost, player.getKiller());
+    }
+
+    @Test
+    void testPlayerCollidingWithPellet() {
+        playerCollisions.collide(player, pellet);
+        verify(pointCalculator).consumedAPellet(player, pellet);
+        verify(pellet).leaveSquare();
+    }
+
+    @Test
+    void testPelletCollidingWithPlayer() {
+        playerCollisions.collide(pellet, player);
+        verify(pointCalculator).consumedAPellet(player, pellet);
+        verify(pellet).leaveSquare();
+    }
 }
